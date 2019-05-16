4fb43d03b12fea71384623b4d46844be052a264e
JaimeMatias <matias.raul.jaime@gmail.com>
2019-05-16 19:28:33 -0300

    test_subconjunto

--- tests/test_subconjunto.py
+++ tests/test_subconjunto.py
@@ -1,6 +1,14 @@
 from Integracion_Continua.read import read_ar
 from Integracion_Continua.subconjunto import subconjuntoclase
-archivo= read_ar('datos.csv')
-#print(archivo)
-var=  subconjuntoclase(archivo[1])
-print(var)
+
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+    def test_subconjuntoclase(self):
+        archivo = read_ar('datos_prueba.csv')
+        var = subconjuntoclase(archivo[1])
+        valor=(['no', 'yes'], [6, 7])
+        self.assertEqual(var,valor)
+
+if __name__ == '__main__':
+    unittest.main()
